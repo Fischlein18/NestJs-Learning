a83f3d545514844fcf6309ab87823c82300814a7
GLOBAL\d042822 <tao.zhang@sap.com>
2021-06-04 03:32:26 +0200

    orders entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
 import { typeOrmConfig } from './config/typeorm.config';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
   imports: [
       TypeOrmModule.forRoot(typeOrmConfig),
-      BooksModule
+      BooksModule,
+      OrdersModule
   ],
   controllers: [AppController],
   providers: [AppService],
